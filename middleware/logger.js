7491ce4d0c905b97eff6523d815943a940384e7c
evuru <evurustanle@gmail.com>
2023-11-18 04:58:22 +0100

    Revert "Project setup, start, first Model plugged in"
    This reverts commit 768ac43dd014cd3e6feaf1b33fdfd3a8f0688a8f.
    
    Genesis

--- middleware/logger.js
+++
@@ -1,9 +0,0 @@
-//@desc     Logs requests to console
-// custom logging middleware just trying stuff
-const logger = (req,res,next)=>{
-    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.url}`);
-    next();
-}
-
-
-module.exports = logger;
